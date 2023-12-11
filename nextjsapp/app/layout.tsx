669b59071e7ea1ec57076368f33dbdcdf8ff6f82
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2023-12-11 18:08:43 -0500

    Changed tab title and description preview

--- nextjsapp/app/layout.tsx
+++ nextjsapp/app/layout.tsx
@@ -12,10 +12,10 @@ import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Agentboard',
+    template: `%s - Agentboard`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'Try out AI agents in your browser',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: 'white' },
     { media: '(prefers-color-scheme: dark)', color: 'black' }
