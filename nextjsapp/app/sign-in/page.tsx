28ea7ed5bb306b3cb405e5ec0bdfd88348aea3e2
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2023-12-18 17:59:19 -0500

    Google oauth seems to be working

--- nextjsapp/app/sign-in/page.tsx
+++ nextjsapp/app/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from '@/auth'
-import { LoginButton } from '@/components/login-button'
+import { GithubLoginButton, GoogleLoginButton } from '@/components/login-button'
 import { redirect } from 'next/navigation'
 
 export default async function SignInPage() {
@@ -9,8 +9,9 @@ export default async function SignInPage() {
     redirect('/')
   }
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
-      <LoginButton />
+    <div className="flex flex-col h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
+      <GoogleLoginButton />
+      <GithubLoginButton />
     </div>
   )
 }
--- nextjsapp/components/login-button.tsx
+++ nextjsapp/components/login-button.tsx
@@ -5,19 +5,23 @@ import { signIn } from 'next-auth/react'
 
 import { cn } from '@/lib/utils'
 import { Button, type ButtonProps } from '@/components/ui/button'
-import { IconGitHub, IconSpinner } from '@/components/ui/icons'
+import { IconGitHub, IconSpinner, IconGoogle } from '@/components/ui/icons'
 
-interface LoginButtonProps extends ButtonProps {
+interface GithubLoginButtonProps extends ButtonProps {
   showGithubIcon?: boolean
   text?: string
 }
+interface GoogleLoginButtonProps extends ButtonProps {
+  showGoogleIcon?: boolean
+  text?: string
+}
 
-export function LoginButton({
+export function GithubLoginButton({
   text = 'Login with GitHub',
   showGithubIcon = true,
   className,
   ...props
-}: LoginButtonProps) {
+}: GithubLoginButtonProps) {
   const [isLoading, setIsLoading] = React.useState(false)
   return (
     <Button
@@ -35,6 +39,34 @@ export function LoginButton({
         <IconSpinner className="mr-2 animate-spin" />
       ) : showGithubIcon ? (
         <IconGitHub className="mr-2" />
+      ) : null}
+      {text}
+    </Button>
+  )
+}
+export function GoogleLoginButton({
+  text = 'Login with Google',
+  showGoogleIcon = true,
+  className,
+  ...props
+}: GoogleLoginButtonProps) {
+  const [isLoading, setIsLoading] = React.useState(false)
+  return (
+    <Button
+      variant="outline"
+      onClick={() => {
+        setIsLoading(true)
+        // next-auth signIn() function doesn't work yet at Edge Runtime due to usage of BroadcastChannel
+        signIn('google', { callbackUrl: `/` })
+      }}
+      disabled={isLoading}
+      className={cn(className)}
+      {...props}
+    >
+      {isLoading ? (
+        <IconSpinner className="mr-2 animate-spin" />
+      ) : showGoogleIcon ? (
+        <IconGoogle className="mr-2" />
       ) : null}
       {text}
     </Button>
