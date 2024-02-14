bca5a2b14a15ee6e73139802f33b0e562800f909
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2024-02-14 10:26:33 -0500

    Modified create-sandbox to use the new code execution environment and add the correct metadata

--- nextjsapp/app/api/create-sandbox/route.ts
+++ nextjsapp/app/api/create-sandbox/route.ts
@@ -1,5 +1,5 @@
 
-import { Sandbox }  from '@e2b/sdk'
+import { Sandbox }  from 'e2b'
 import { cookies } from 'next/headers';
 import { createClient } from '@/utils/supabase/server'
 
@@ -13,76 +13,32 @@ export async function GET() {
         })
     }
 
-    if(process.env.DOCKER == 'local'){
+    try {
+        let sandbox = await Sandbox.create({
+            template: 'ois-code-execution-sandbox',
+            metadata: { userID: user.id }
+        })
+        if (process.env.NODE_ENV === 'development') {
+            await sandbox.keepAlive(5 * 60 * 1000) 
+        }
+        else {
+            await sandbox.keepAlive(60 * 60 * 1000) 
+        }
 
-        return new Response(JSON.stringify({ sandboxID: 'dummySandboxID' }), {
+        await sandbox.close()
+
+        console.log("Sandbox created, id: ", sandbox.id)
+        return new Response(JSON.stringify({ sandboxID: sandbox.id }), {
             headers: {
                 'Content-Type': 'application/json'
             },
             status: 200
         })
     }
-    else{
-        try {
-
-            /*
-            Google Cloud Run gets "warmed up" when it receives a request, so we send a dummy request to the API to speed up the first 
-            actual request the user makes after the sandbox is finished starting
-            */
-            fetch('https://proxy-rotps5n5ja-uc.a.run.app/v1/chat/completions', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    model: 'gpt-3.5-turbo',
-                    messages: [{
-                        content: 'This is a test, respond with Hello',
-                        role: 'user'
-                    }],
-                    max_tokens: 10
-                })
-            }).then(response => response.json())
-              .then(data => console.log(data))
-              .catch(error => console.error('Error:', error));
-
-
-            let sandbox
-            if (process.env.NODE_ENV === 'development') {
-                sandbox = await Sandbox.create({ 
-                    template: 'e2b-ois-image-dev',
-                })
-                await sandbox.keepAlive(5 * 60 * 1000) 
-            }
-            else {
-                sandbox = await Sandbox.create({ 
-                    template: 'e2b-ois-image',
-                })
-                await sandbox.keepAlive(5 * 60 * 1000) 
-            }
-
-            await sandbox.process.start({
-                cmd: `uvicorn --app-dir /code server:app --host 0.0.0.0 --port 8080`,
-            })
-            
-            await new Promise(resolve => setTimeout(resolve, 3000));
-
-            await sandbox.close()
-
-            console.log("Sandbox created, id: ", sandbox.id)
-            return new Response(JSON.stringify({ sandboxID: sandbox.id }), {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                status: 200
-            })
-        }
-        catch (e){
-            return new Response((JSON.stringify(e)), {
-                status: 500
-            })
-        }
-
+    catch (e){
+        return new Response((JSON.stringify(e)), {
+            status: 500
+        })
     }
 
 }
