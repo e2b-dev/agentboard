9e73a709e2818c1ba6ec2f9a814b7047d9a580c3
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2024-03-05 14:00:43 -0500

    Added a CORS server header for www.agentboard.dev

--- backend/server.py
+++ backend/server.py
@@ -183,6 +183,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "https://agentboard.dev",
+    "https://www.agentboard.dev",
     "https://agentboard-git-staging-e2b.vercel.app",
     "https://agentboard-git-dev-e2b.vercel.app",
     "https://agentboard-git-gce-refactor-e2b.vercel.app"
