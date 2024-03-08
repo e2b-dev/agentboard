0e6d90caad23c24e53d65c612947cbb3685d5988
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-03-08 19:02:48 +0100

    Remove router push to root if there's auth error

--- nextjsapp/components/login-button.jsx
+++ nextjsapp/components/login-button.jsx
@@ -1,13 +1,11 @@
 'use client'
 
 import * as React from 'react'
-import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { IconGitHub, IconSpinner, IconGoogle } from '@/components/ui/icons'
 import { createClient } from '@/utils/supabase/client'
 
-export function GithubLoginButton({ ...props}) {
-  const router = useRouter()
+export function GithubLoginButton({ ...props }) {
   const supabase = createClient()
   const [isLoading, setIsLoading] = React.useState(false)
 
@@ -19,17 +17,14 @@ export function GithubLoginButton({ ...props}) {
         setIsLoading(true)
         const { error } = await supabase.auth.signInWithOAuth({
           provider: 'github',
-            options: {
-              redirectTo: window.location.origin + '/auth/callback'
-            }
+          options: {
+            redirectTo: window.location.origin + '/auth/callback'
+          }
         })
-        if (error){
+        if (error) {
           console.error('Error signing in:', error.message)
           setIsLoading(false)
         }
-        
-        else router.push('/')
-
       }}
       disabled={isLoading}
       {...props}
@@ -38,15 +33,14 @@ export function GithubLoginButton({ ...props}) {
         <IconSpinner className="mr-2 animate-spin" />
       ) : (
         <IconGitHub className="mr-2" />
-      ) }
+      )}
       {"Login with Github"}
     </Button>
   )
 }
-export function GoogleLoginButton({...props}) {
+export function GoogleLoginButton({ ...props }) {
   const supabase = createClient()
   const [isLoading, setIsLoading] = React.useState(false)
-  const router = useRouter()
 
   return (
     <Button
@@ -56,15 +50,14 @@ export function GoogleLoginButton({...props}) {
         setIsLoading(true)
         const { error } = await supabase.auth.signInWithOAuth({
           provider: 'google',
-            options: {
-              redirectTo: window.location.origin + '/auth/callback'
-            }
+          options: {
+            redirectTo: window.location.origin + '/auth/callback'
+          }
         })
-        if (error){
+        if (error) {
           console.error('Error signing in:', error.message)
           setIsLoading(false)
         }
-        else router.push('/')
       }}
       disabled={isLoading}
       {...props}
@@ -73,7 +66,7 @@ export function GoogleLoginButton({...props}) {
         <IconSpinner className="mr-2 animate-spin" />
       ) : (
         <IconGoogle className="mr-2" />
-      ) }
+      )}
       {"Login with Google"}
     </Button>
   )
