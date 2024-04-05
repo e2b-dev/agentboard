07d2729582f6b5e43e5db5e232bcf5f98aaeb3d9
Jakub Novak <jakub@e2b.dev>
2024-04-04 17:54:22 -0700

    Styling icons

--- frontend/components/ui/icons.tsx
+++ frontend/components/ui/icons.tsx
@@ -215,13 +215,23 @@ function IconUpload({ className, ...props }: React.ComponentProps<'svg'>) {
       className={cn('h-4 w-4', className)}
       {...props}
     >
+      <style>
+        {`
+          #icon-upload {
+           fill: black
+          }
+          @media (prefers-color-scheme: dark) {
+            #icon-upload { fill: white }
+          }`}
+      </style>
       <path
+        id="icon-upload"
         d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,12a1,1,0,0,0-1,1v6a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V13a1,1,0,0,0-2,0v6a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V13A1,1,0,0,0,21,12Z"
-        fill="white"
       ></path>
     </svg>
   )
 }
+
 function IconDownload({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
     <svg
@@ -282,14 +292,14 @@ function IconOpenInterpreter({
     >
       <style>
         {`
-          circle {
+          #open-interpreter {
            fill: white
           }
           @media (prefers-color-scheme: dark) {
-            circle { fill: black }
+            #open-interpreter { fill: black }
           }`}
       </style>
-      <circle fill="inherit" cx="50" cy="50" r="50" />
+      <circle id="open-interpreter" fill="inherit" cx="50" cy="50" r="50" />
     </svg>
   )
 }
